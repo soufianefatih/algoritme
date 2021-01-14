e6259b7b1677594e78ba6f8fa0521dec7e3d7d91
soufiane fatih <fatihe.soufiane@gmail.com>
2021-01-14 10:16:31 +0100

    tableau_multip

---
+++ SansNom9.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+  int main () {
+  	
+  	int n,i;
+  	printf("enter la valeur de n : \n");
+  	scanf("%d",&n);
+  	
+  	for (i=1;i<=10;i++){
+  		
+  		printf("%d x %d = %d \n",i,n,i*n);
+  		
+  		
+  		
+  		
+	  } 	
+  	
+  	
+  }
